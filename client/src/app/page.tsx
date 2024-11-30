1a32bf0fa3185621d1545a21216663e0be5f4da3
sandipkatel <babusandipkatel@gmail.com>
2024-11-30 19:14:44 +0545

    update import on page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import React, { useState, useEffect } from "react";
+import styles from "@/app/page.module.css";
+import NavBar from "@/components/navbar";
+import Hero from "@/components/hero";
 
 export default function Home() {
   const [message, setMessage] = useState("Loading...");
@@ -12,7 +15,10 @@ export default function Home() {
   }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <header className="row-start-1 flex gap-6 items-center justify-center">
+        <NavBar /></header>
+      <main className={styles.mainWrapper}>
+        <Hero />
         {message}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
