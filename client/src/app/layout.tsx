d6b13b104f19e214d1ec9bcaf96b423feac24221
sandipkatel <babusandipkatel@gmail.com>
2024-12-01 22:07:01 +0545

    update background and foreground

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="color-scheme" content="light dark" />
+      </head>
       <body
         className="antialiased"
       >
